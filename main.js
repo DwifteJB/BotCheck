6706dce6312a1bb4baa3bfd3f66ecbadbd96a9d7
DwifteJB <dwifte@outlook.com>
2021-02-13 16:35:49 +0000

    fixes shit

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 const { Menu, app, BrowserWindow, shell, Notification, } = require("electron");
 const fs = require("fs");
+const fetch = require("node-fetch")
 const path = require("path");
+let dataPath;
+if (process.platform === "win32") dataPath = process.env.APPDATA;
+else if (process.platform === "darwin") dataPath = path.join(process.env.HOME, "Library", "Preferences");
+else dataPath = process.env.XDG_CONFIG_HOME ? process.env.XDG_CONFIG_HOME : path.join(process.env.HOME, ".config");
+dataPath = path.join(dataPath, "BotCheck") + "/";
+let PluginPath = path.join(dataPath, "plugins")
+// Install Themer //
+if (!fs.existsSync(PluginPath + "/themeinjector.plugin.js")) {
+(async () => {
+  const res = await fetch("https://raw.githubusercontent.com/DwifteJB/BotCheck/plugins/plugins/themeinjector.plugin.js");
+  const fileStream = fs.createWriteStream(PluginPath + "/themeinjector.plugin.js");
+  await new Promise((resolve, reject) => {
+      res.body.pipe(fileStream);
+      res.body.on("error", reject);
+      fileStream.on("finish", resolve);
+    });
+})();
+}
 const template = [
 
   {
@@ -75,50 +94,30 @@ function createWindow() {
     // win.webContents.openDevTools()
     win.setMenuBarVisibility(false)
 }
-app.whenReady().then(createWindow)
 app.whenReady().then(() => {
-    let dataPath;
-    if (process.platform === "win32") dataPath = process.env.APPDATA;
-    else if (process.platform === "darwin") dataPath = path.join(process.env.HOME, "Library", "Preferences");
-    else dataPath = process.env.XDG_CONFIG_HOME ? process.env.XDG_CONFIG_HOME : path.join(process.env.HOME, ".config");
-    dataPath = path.join(dataPath, "BotCheck") + "/";
-    const PluginPath = path.join(dataPath, "plugins")
-    let contextMenu = Menu.buildFromTemplate(template);
-
-    // Install Themer //
-      if (!fs.existsSync(PluginPath + "/themeinjector.plugin.js")) {
-        (async () => {
-          const res = await fetch("https://raw.githubusercontent.com/DwifteJB/BotCheck/plugins/plugins/themeinjector.plugin.js");
-          const fileStream = fs.createWriteStream(PluginPath + "/themeinjector.plugin.js");
-          await new Promise((resolve, reject) => {
-              res.body.pipe(fileStream);
-              res.body.on("error", reject);
-              fileStream.on("finish", resolve);
-            });
-        });
-      }
-      const plugins = fs.readdirSync(PluginPath).filter(file => file.endsWith('.plugin.js'));
-      for (const file of plugins) {
-        const loaded = require(`${PluginPath}/${file}`);
-        template[0].submenu.splice(1, 0, {label: loaded.name, click: async () => {
-          const notification = {
-            title: "BotCheck Plugin Inspect",
-            body: `Plugin info for ${loaded.name} by ${loaded.author}\n${loaded.description}`
-          }
-          new Notification(notification).show()
-        }});
-        render();
-      }
-      function render() {
-        contextMenu = Menu.buildFromTemplate(template);
-        Menu.setApplicationMenu(contextMenu)
-      }
+  createWindow();
+  let contextMenu = Menu.buildFromTemplate(template);
+  for (const file of fs.readdirSync(PluginPath).filter(file => file.endsWith('.plugin.js'))) { 
+      const loaded = require(`${PluginPath}/${file}`);
+      template[0].submenu.splice(1, 0, {label: loaded.name, click: async () => {
+        const notification = {
+          title: "BotCheck Plugin Inspect",
+          body: `Plugin info for ${loaded.name} by ${loaded.author}\n${loaded.description}`
+        }
+        new Notification(notification).show()
+    }});
+    render();
+  }
+  function render() {
+    contextMenu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(contextMenu)
+  }
       
 })
 app.on("window-all-closed", () => {
     try {
       fs.unlinkSync("./commands.json")
-    } catch { }
+    } catch {}
     if (process.platform != "darwin") {
         app.quit()
     }
--- src/js/main.js
+++ src/js/main.js
@@ -21,6 +21,7 @@ else if (process.platform === "darwin") dataPath = path.join(process.env.HOME, "
 else dataPath = process.env.XDG_CONFIG_HOME ? process.env.XDG_CONFIG_HOME : path.join(process.env.HOME, ".config");
 dataPath = path.join(dataPath, "BotCheck") + "/";
 let PluginPath = path.join(dataPath, "plugins")
+let ThemePath = path.join(PluginPath, "themes")
 if (!fs.existsSync(dataPath)) fs.mkdirSync(dataPath);
 if (!fs.existsSync(PluginPath)) fs.mkdirSync(PluginPath);
 if (!fs.existsSync(ThemePath)) fs.mkdirSync(ThemePath);
