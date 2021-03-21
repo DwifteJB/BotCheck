12ae5e7df40475d3d0f66418e9f926c7987e8acd
CrafterPika <48294025+CrafterPika@users.noreply.github.com>
2021-03-21 15:40:20 +0100

    tes123

--- plugins/eruda.plugin.js
+++ plugins/eruda.plugin.js
@@ -5,7 +5,7 @@ module.exports = {
   description: "loads Eruda's JS Console into the app.",
   author: "CrafterPika, liriliri",
   async execute() {
-    var script = document.createElement('script'); script.src="https://raw.githubusercontent.com/DwifteJB/BotCheck/plugins/assets/eruda.js";
+    var script = document.createElement('script'); script.src="https://raw.githubusercontent.com/DwifteJB/BotCheck/plugins/assets/eruda.js"; script.type="application/javascript";
     document.body.appendChild(script); script.onload = function () { eruda.init() };
   },
 }
